139349837d137990bc98c719a79301a0df39509b
Odie100 <andriampeno.odilon@gmail.com>
2023-08-11 12:45:16 +0300

    refactor: remove unused variables

--- main2.py
+++ main2.py
@@ -1,24 +1,25 @@
 import random
 import pygame
 
-width = 20
-heigth = 15
-cell_size = 40
+BLOCK_WIDTH = 30
+BLOCK_HEIGTH = 15
+CELL_SIZE = 30
+SCREEN_WIDTH = BLOCK_WIDTH * CELL_SIZE
+SCREEN_HEIGTH = BLOCK_HEIGTH * CELL_SIZE
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGTH))
+RUNNING = True
+FPS = 30
 
 pygame.init()
-w = width * cell_size
-h = heigth * cell_size
-screen = pygame.display.set_mode((w,h))
-clock = pygame.time.Clock()
 
+clock = pygame.time.Clock()
 grid = []
 
-for y in range(heigth+1):
-    for x in range(width):
+for y in range(BLOCK_HEIGTH+1):
+    for x in range(BLOCK_WIDTH):
         grid.append((x,y))
 
-random.shuffle(grid)
-
+# random.shuffle(grid)
 sets = [{cell} for cell in grid]
 
 def find_set(cell):
@@ -35,36 +36,35 @@ def merge_sets(set1,set2):
 def draw_grid():
     for cell in grid:
         x,y = cell
-        pygame.draw.rect(screen,(0,0,0), (x*cell_size,y*cell_size,cell_size,cell_size),1)
+        pygame.draw.rect(SCREEN,(0,0,0), (x*CELL_SIZE,y*CELL_SIZE,CELL_SIZE,CELL_SIZE),1)
 
 def draw_horizontal_wall(cell):
-    print("horizontal wall")
     x,y = cell
-    pygame.draw.line(screen,(0,0,0),(x*cell_size,y*cell_size+cell_size),((x+1)*cell_size,y*cell_size+cell_size),6)
+    pygame.draw.line(SCREEN,(0,random.randrange(50,255),0),(x*CELL_SIZE,y*CELL_SIZE+CELL_SIZE),((x+1)*CELL_SIZE,y*CELL_SIZE+CELL_SIZE),6)
 
 def draw_vertical_wall(cell):
-    print("draw vertical wall")
     x,y = cell
-    pygame.draw.line(screen,(0,0,0),(x*cell_size+cell_size,y*cell_size),(x*cell_size+cell_size,(y+1)*cell_size),6)
+    pygame.draw.line(SCREEN, (0,random.randrange(50,255),0),(x*CELL_SIZE+CELL_SIZE,y*CELL_SIZE),(x*CELL_SIZE+CELL_SIZE,(y+1)*CELL_SIZE),6)
 
-running = True
+def random_color():
+    red = random.randint(0,255)
+    green = random.randint(0,255)
+    blue = random.randint(0,255)
+    return (red,green,blue)
 
-screen.fill((255,255,255))
+SCREEN.fill((0,0,0))
 draw_grid()
-while running:
+while RUNNING:
+    edges = []
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-    
-
-    edges = []
+            RUNNING = False
 
     for cell in grid:
         x,y = cell
-        
-        if x < width -1:
+        if x < BLOCK_WIDTH -1:
             edges.append(((x,y), (x+1,y)))
-        if y < heigth:
+        if y < BLOCK_HEIGTH:
             edges.append(((x,y), (x,y+1)))
 
     random.shuffle(edges)
@@ -74,21 +74,17 @@ while running:
         # print(f"cell1: {cell1}\ncell2:{cell2}")
         set1 = find_set(cell1)
         set2=find_set(cell2)
-        # print(f"set1:{set1}\nset2:{set2}")
-
         # if set1 != set2 and set1 is not None and set2 is not None:
         if set1 != set2:
             merge_sets(set1,set2)
             x1,y1 = cell1
             x2,y2 = cell2
-
             if x1 == x2:
                 draw_horizontal_wall(cell1)
             elif y1 == y2:
                 draw_vertical_wall(cell1)
 
-
     pygame.display.flip()
-    clock.tick(30)
+    clock.tick(FPS)
 
 pygame.quit()
